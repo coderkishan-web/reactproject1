9b8985d78fd1b6a3740e3adb4b47ad9bed0b6d9c
coderkishan-web <shindekishan937@gmail.com>
2024-09-13 18:36:15 +0530

    react project modified

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -8,7 +8,7 @@ const Demo = () => {
                 <div className='col-1'>
                     <p>More Than 100 Financial Planners,</p>
                     <p>One Philosophy</p>
-                    <p>Every single one of our financial advisors receives rigorous training according to John Doe’s philosophy based on academic research (including that of a Nobel laureate in Economics) and Behavioral Finance.</p>
+                    <p>Every single one of our financial advisors receives rigorous training according to kishan shinde’s philosophy based on academic research (including that of a Nobel laureate in Economics) and Behavioral Finance.</p>
                     <button className='button'>Get your free financial analysis</button>
                 </div>
                 <div className='col-2'>
